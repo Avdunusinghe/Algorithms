eff980c2bc8bdc474e10cfe7bf0fdd75bb9fe84a
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-11 15:44:07 +0530

    InsertSort Algorithm implemented

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-#Write a program to read a set of numbers (between 10 to 20) from the keyboard and store them in an array.
+# Write a program to read a set of numbers (between 10 to 20) from the keyboard and store them in an array.
 
-#Declare Array
-a=[]
-#input Array Size
-n=int(input("Enter the number of element: "))
+# Declare Array
+a = []
+# input Array Size
+n = int(input("Enter the number of element: "))
 
-#save Array Element
+# save Array Element
 
-for v in range(0,n):
+for v in range(0, n):
     a.append(int(input("Enter a number")))
 
-#print array elements
+# print array elements
 print(a)
 
 
+# Sort the numbers in ascending order with the Insertion sorting algorithm
 
+def insert_sort(a):
+    for j in range(1, len(a)):
+        key = a[j]
+        i = j - 1
+        while i <= 0 and a[i] > key:
+            a[i + 1] = a = [i]
+            i = i - 1
+        a[i + 1] = key
+
+
+# calling function
+insert_sort(a)
+
+print("Sorted Array")
+print(a)
